e31a0c37c06c475bd2adcde7a9d7e796f84ded19
Danny Pham <dannynpham11@gmail.com>
2022-04-08 02:47:52 -0700

    Disabled warning.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="dark:bg-[#0D2436] dark:text-white bg-gray-300 app">
+      {/* @ts-ignore */}
       <Component {...pageProps} />
     </div>
   );
