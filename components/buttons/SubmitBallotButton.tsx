3a6978208e0a3ac38e0413fb271d338d5f78410d
Danny Pham <dannynpham11@gmail.com>
2022-04-07 22:29:04 -0700

    Added a default on submit.

--- components/buttons/SubmitBallotButton.tsx
+++ components/buttons/SubmitBallotButton.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 
-const SubmitBallotButton = () => {
+const SubmitBallotButton = (props: any) => {
+  const { onSubmit = () => {} } = props;
+
   return (
     <button
       className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded fixed right-10 bottom-10"
-      type="submit"
+      onClick={onSubmit}
     >
       Submit Ballot Button
     </button>
